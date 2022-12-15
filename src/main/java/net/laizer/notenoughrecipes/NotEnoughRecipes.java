9b582a96c3c400cf5e15cd7c3e3fe62e7d01fa44
BuildTools <unconfigured@null.spigotmc.org>
2022-12-16 01:19:08 +0400

    Initial commit

---
+++ src/main/java/net/laizer/notenoughrecipes/NotEnoughRecipes.java
@@ -0,0 +1,15 @@
+package net.laizer.notenoughrecipes;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NotEnoughRecipes implements ModInitializer {
+	public static final String MOD_ID = "notenoughrecipes";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/laizer/notenoughrecipes/NotEnoughRecipesClient.java
@@ -0,0 +1,10 @@
+package net.laizer.notenoughrecipes;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotEnoughRecipesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
